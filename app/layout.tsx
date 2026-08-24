dbf73aef42f328614b07437a20a19c73b5717455
Mercy1o <kototsiaro@gmail.com>
2026-08-24 13:05:01 -0400

    revert: remove restored brand intro from simple template

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import "./mobile.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import BrandIntro from "@/components/BrandIntro";
 import { siteConfig } from "@/data/site";
 
 const displaySans = Inter({
@@ -60,7 +59,6 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       <body className={`${displaySans.variable} ${mono.variable}`}>
         <a href="#main-content" className="skip-link">Skip to content</a>
         <Navbar />
-        <BrandIntro />
         <div id="main-content">{children}</div>
         <Footer />
       </body>
