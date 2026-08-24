22cac9f96e417f425898b63f4c59d8d9febb2ad7
Mercy1o <kototsiaro@gmail.com>
2026-08-24 13:04:49 -0400

    revert: return navbar to simple white template state

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,35 +4,15 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { siteConfig } from "@/data/site";
 
-function BrandWordmark() {
-  return (
-    <>
-      <span className="brand-trnsk">trnsk</span>
-      <span className="brand-design">Design</span>
-    </>
-  );
-}
-
 export default function Navbar() {
   const pathname = usePathname();
-  const isHome = pathname === "/";
 
   return (
     <header className="site-nav">
       <div className="site-shell site-nav-inner">
-        {isHome ? (
-          <span
-            data-brand-target
-            aria-hidden="true"
-            className="site-brand site-brand-target"
-          >
-            <BrandWordmark />
-          </span>
-        ) : (
-          <Link href="/" className="site-brand" aria-label={`${siteConfig.brand} home`}>
-            <BrandWordmark />
-          </Link>
-        )}
+        <Link href="/" className="site-brand" aria-label={`${siteConfig.brand} home`}>
+          {siteConfig.brand}
+        </Link>
 
         <nav aria-label="Main navigation" className="site-nav-links">
           {siteConfig.navigation.map((item) => {
